6f5c1c66ac57c116bd1760fa6a0d66884c6b040e
dehi24 <davifa16@yahoo.com>
2014-10-14 09:54:46 -0600

    basurin

--- SAPR/SAPR/FormularioUsuario.aspx.cs
+++ SAPR/SAPR/FormularioUsuario.aspx.cs
@@ -23,8 +23,10 @@ namespace SAPR
 
         protected void btnAgregarUsuario_Click(object sender, EventArgs e)
         {
-           
-            controladora.insertarUsuario(this.txtNombreUsuario.Value.ToString(), this.txtCedula.Value.ToString(), this.textEmail.Value.ToString(), this.textTelefono.Value.ToString(), this.textCelular.Value.ToString(), this.cmbRoles.SelectedItem.ToString());
+            limpiarCampos();
+            modo = 1;
+            irAModo();
+            //controladora.insertarUsuario(this.txtNombreUsuario.Value.ToString(), this.txtCedula.Value.ToString(), this.textEmail.Value.ToString(), this.textTelefono.Value.ToString(), this.textCelular.Value.ToString(), this.cmbRoles.SelectedItem.ToString());
             gridUsuarios.DataBind();
         }
 
@@ -63,6 +65,58 @@ namespace SAPR
 
         protected void btnAceptar_Click(object sender, EventArgs e){
 
+            String []resultado = new String[1];
+
+            if (modo == 1) // si se quiere insertar
+            {
+                resultado = controladora.insertarUsuario(this.txtNombreUsuario.Value.ToString(), this.txtCedula.Value.ToString(), this.textEmail.Value.ToString(), this.textTelefono.Value.ToString(), this.textCelular.Value.ToString(), this.cmbRoles.SelectedItem.ToString());
+
+                if (resultado[0] == "")
+                { // si inserto el proveedor : va a modo consultar con ese proveedor
+                    proveedorConsultado = controladora.consultarProveedor(this.textCedula.Value.ToString());
+                    modo = 4;
+                    habilitarCampos(false);
+                } // si no lo inserto no debe cambiar de modo ni limpiar la pantalla
+            }
+            else if (modo == 2)//si se quiere modificar
+            {
+                Boolean res = true;
+                Object[] datosMod = obtenerDatosFormulario();
+                String[] result = controladora.modificarProveedor(datosMod, proveedorConsultado);
+                mostrarMensaje(result[0], result[1], result[2]); // se muestra el resultado
+                if (result[0].Contains("success"))// si fue exitoso
+                {
+                    llenarGrid(); //se actualiza el grid con el nuevo proveedor incluido
+                }
+                else if (result[2].Contains("cedula ingresada ya existe")) // si ya hay uno con esa cédula
+                {
+                    res = false;
+                    modo = 1; //no se cambia de modo y retorna false
+                }
+                proveedorConsultado = controladora.consultarProveedor(this.textCedula.Value.ToString());
+                llenarGrid();
+                modo = 4;
+                irAModo();
+                // se recuperan los datos que se hallan ingresado(existe un método para esto)
+
+                // se le pide a la controladora que modifique el proveedor
+
+                // se muestra el resultado de la modificación(revisar otros métodos para ver como mostrar el error devuelto)
+
+                // se actualiza el proveedor que se consultó por última vez, es decir, se consulta el proveedor recien modificado
+                //se carga el proveedor modificado en el grid(esto es volver a llenar el grid)
+                // se cambia a modo consultar con el proveedor modificado
+            }
+            else if (modo == 3)// si se quiere eliminar
+            {
+                limpiarCampos(); // se limpian los campos
+                modo = 0;//se va a modo reset, y no se elimina porque existe un modal de confirmacion que se muestra
+            }
+            if (operacionCorrecta)// si se logró la operación
+            {
+                irAModo();// se cambia de modo y actualiza el grid
+                llenarGrid();
+            }
         }
 
         protected void btnCancelar_Click(object sender, EventArgs e)
@@ -96,7 +150,6 @@ namespace SAPR
                 btnAceptar.Enabled = true;
                 btnCancelar.Enabled = true;
                 btnModificarUsuario.Enabled = false;
-                btnAgregarUsuario.Enabled = true;
                 btnEliminarUsuario.Enabled = false;
 
             }
