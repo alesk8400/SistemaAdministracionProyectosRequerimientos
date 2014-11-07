dbf996b374f0324aded48d8986dc250dd84d1638
gerjo19 <gertri5@gmail.com>
2014-11-07 10:48:42 -0600

    Documentación

--- SAPR/SAPR/App_Code/Controladoras/ControladoraEstructura.cs
+++ SAPR/SAPR/App_Code/Controladoras/ControladoraEstructura.cs
@@ -7,14 +7,25 @@ using SAPR.App_Code.Entidades;
 
 namespace SAPR.App_Code.Controladoras
 {
+    /*
+     Clase controladora de estructura que maneja los métodos para Sprints y módulos
+     */
     public class ControladoraEstructura{
         ControladoraBDEstructura controladoraBDEstructura;
         ControladoraProyecto controladoraProyecto;
 
+        /*
+            Constructor que inicializa el objeto controladoraBDEstructura y controladoraProyecto para manajar base de datos y solicitar datos de proyecto
+         */
         public ControladoraEstructura() {
             controladoraBDEstructura = new ControladoraBDEstructura();
             controladoraProyecto = new ControladoraProyecto();
         }
+
+        /*
+         Método que recibe nombre, descripción y el nombre del proyecto, deja los datos del Sprint en un objeto vector que es encapsulado en la entidad Sprint, 
+         se solicita el ID de Proyecto con el nombre y se manda la controladora BD a insertar un Sprint.
+         */
         public String[] insertarSprint(String nombre, String descripcion,String nombreProyecto)
         {
             Object[] datos = new Object[2];  //Se crea un objeto con los datos del sprint
@@ -26,6 +37,11 @@ namespace SAPR.App_Code.Controladoras
             return controladoraBDEstructura.insertarSprint(sprint, idProyecto);
         }
 
+        /*
+         Método que recibe nombre, descripción, el nombre del proyecto y la entidad del Sprint viejo, deja los datos del Sprint en un objeto vector que es
+         encapsulado en la entidad Sprint, se solicita el ID de Proyecto con el nombre y se manda la controladora BD a modificar enviandole el Sprint nuevo, 
+         el id del proyecto y el nombre del sprint viejo.
+         */
         public string[] modificarSprint(String nombre, String descripcion, String nombreProyecto,EntidadSprint sprintViejo)
         {
             Object[] datos = new Object[2];
@@ -37,6 +53,9 @@ namespace SAPR.App_Code.Controladoras
             return controladoraBDEstructura.modificarSprint(sprintNuevo, idProyecto, sprintViejo.Nombre);
         }
 
+        /*
+         Método que recibe nombre del sprint y el nombre del proyecto, solicita el ID de Proyecto con el nombre y se manda la controladora BD a eliminar 
+         */
         public String[] eliminarSprint(String nombreSprint, String nombreProyecto)
         { 
             int idProyecto = 0;
@@ -44,6 +63,9 @@ namespace SAPR.App_Code.Controladoras
             return controladoraBDEstructura.eliminarSprint(nombreSprint,idProyecto);
         }
 
+        /*
+         Método que solicita los datos de un Sprint a la controladora de BD, los encapsula en la entidad y lo retorna, recibiendo el nombre del sprint y el nombre del proyecto
+         */
         public EntidadSprint consultarSprint(String nombreSprint,String nombreProyecto)
         {  
             EntidadSprint sprint = null; //para encapsular los datos consultados.
@@ -63,6 +85,10 @@ namespace SAPR.App_Code.Controladoras
             return sprint;
         }
 
+        /*
+         Método que recibe nombre, descripción, nombre del sprint y el nombre del proyecto, deja los datos del Sprint en un objeto vector que es encapsulado en la entidad módulo, 
+         se solicita el ID del Sprint con el nombre y nombre de proyecto y se manda la controladora BD a insertar un Módulo.
+         */
         public String[] insertarModulo(String nombre, String descripcion, String nombreSprint, String nombreProyecto)
         {
             Object[] datos = new Object[2];  //Se crea un objeto con los datos del sprint
@@ -74,11 +100,21 @@ namespace SAPR.App_Code.Controladoras
             return controladoraBDEstructura.insertarModulo(modulo, idSprint);
         }
 
+        /*
+         Método que recibe el nombre del sprint y el nombre del proyecto, manda la controladora de proyecto a obtener el ID y a la controladora BD a obtener el ID con el nombre 
+         del Sprint y el id del proyecto
+         */
         public int getIdSprint(String nombreSprint, String nombreProyecto) { //Requiere el proyecto para que en la consulta solo busque los sprints de ese proyecto. (No debe haber nombres de sprints iguales en un mismo proyecto)
             int idProyecto = 0;
             idProyecto = controladoraProyecto.getIdProyecto(nombreProyecto);
             return controladoraBDEstructura.getIdSprint(nombreSprint, idProyecto);
         }
+
+        /*
+         Método que recibe nombre, descripción, nombre del sprint, nombre del proyecto y la entidad del Módulo viejo, deja los datos del Módulo en un objeto vector que es
+         encapsulado en la entidad Módulo, se solicita el ID del Sprint con el nombre y se manda la controladora BD a modificar enviandole el Módulo nuevo, 
+         el id del Sprint y el nombre del módulo viejo.
+         */
         public string[] modificarModulo(String nombre, String descripcion, String nombreSprint, String nombreProyecto, EntidadModulo moduloViejo) //En un sprint no deben existir módulos con el mismo nombre
         {
             Object[] datos = new Object[2];
@@ -90,6 +126,10 @@ namespace SAPR.App_Code.Controladoras
             return controladoraBDEstructura.modificarModulo(moduloNuevo, idSprint, moduloViejo.Nombre);
         }
 
+        /*
+         Método que recibe nombre del módulo, el nombre del sprint y el nombre del proyecto, solicita el ID del sprint con el nombre y se manda la controladora BD a
+         eliminar el módulo
+         */
         public String[] eliminarModulo(String nombreModulo, String nombreSprint, String nombreProyecto)
         { //Requiere mandar el id del sprint para poder identificar cual módulo se eliminará, con solo el nombre no se puede saber
             int idSprint = 0;
@@ -97,6 +137,9 @@ namespace SAPR.App_Code.Controladoras
             return controladoraBDEstructura.eliminarModulo(nombreModulo, idSprint);
         }
 
+        /*
+         Método que solicita los datos de un Módulo a la controladora de BD con el nombre del módulo y el id del sprint, los encapsula en la entidad y lo retorna
+         */
         public EntidadModulo consultarModulo(String nombreModulo, String nombreSprint, String nombreProyecto)
         { //Requiere mandar el id del sprint para poder identificar cual módulo se consultará, con solo el nombre no se puede saber
             int idSprint = 0;
@@ -116,36 +159,38 @@ namespace SAPR.App_Code.Controladoras
             return modulo;
         }
 
+        /*
+         Método que solicita a la controladora de BD todos los sprints de un proyecto, con su id
+         */
         public DataTable getSprints(int proyecto)
         {
             return controladoraBDEstructura.getSprints(proyecto);
         }
 
-
-
+        /*
+         Método que solicita a la controladora de BD todos los módulo de un sprint, con su id
+         */
         public DataTable getModulo(int sprintId)
         {
             return controladoraBDEstructura.getModulo(sprintId); ;
         }
 
+        /*
+         Método que solicita a la controladora de BD todos los requeriminetos de un módulo, con su id
+         */
         public DataTable getRequerimientos(int moduloId)
         {
             return controladoraBDEstructura.getRequerimientos(moduloId); ;
         }
 
-        /*public DataTable getProyectos()
-        {
-            //controladoraProyecto.geNombreProyectos();
-        }*/
-
+        /*
+         Método que solicita a la controladora de BD todos los nombres de sprint de un proyecto con su id
+         */
         public DataTable getNombresSprint(int proyecto)
         {
-
-            
-            DataTable resultado = crearTablaSprint();
+            DataTable resultado = crearTablaSprint(); //Este método crea la tabla
 
             Object[] datos = new Object[2];
-            //string basura;
             try
             {
                 DataTable consulta = controladoraBDEstructura.getSprints(proyecto);
@@ -153,7 +198,6 @@ namespace SAPR.App_Code.Controladoras
                 {
                     foreach (DataRow fila in consulta.Rows)
                     {
-                        //basura = fila[1].ToString();
                         datos[0] = fila[1].ToString(); // Van los Nombres
                         datos[1] = fila[0].ToString(); // Van los ids del sprint
                         resultado.Rows.Add(datos);// cargar en la tabla los datos de cada usuario
@@ -172,7 +216,9 @@ namespace SAPR.App_Code.Controladoras
 
             return resultado;
         }
-
+         /*
+          Método que crea la tabla de Sprints con solo el nombre y el id como columnas
+          */
         protected DataTable crearTablaSprint()
         {
             DataTable tabla = new DataTable();
@@ -193,7 +239,9 @@ namespace SAPR.App_Code.Controladoras
         }
 
 
-
+        /*
+          Método que crea la tabla de proyectos con solo el nombre y el id como columnas
+          */
         protected DataTable crearTablaProyecto()
         {
             DataTable tabla = new DataTable();
@@ -213,14 +261,14 @@ namespace SAPR.App_Code.Controladoras
             return tabla;
         }
 
-
+        /*
+         Método que solicita a la controladora de BD todos los nombres de un proyecto con su id
+         */
         public DataTable getNombresProyectos()
         {
-
-            DataTable resultado = crearTablaProyecto();
+            DataTable resultado = crearTablaProyecto(); //Este método crea la tabla
 
             Object[] datos = new Object[2];
-            //string basura;
             try
             {
                 DataTable consulta = controladoraProyecto.getProyectos();
@@ -228,7 +276,6 @@ namespace SAPR.App_Code.Controladoras
                 {
                     foreach (DataRow fila in consulta.Rows)
                     {
-                        //basura = fila[1].ToString();
                         datos[0] = fila[1].ToString(); // Van los Nombres
                         datos[1] = fila[0].ToString(); // Van los ids del sprint
                         resultado.Rows.Add(datos);// cargar en la tabla los datos de cada usuario
@@ -248,6 +295,9 @@ namespace SAPR.App_Code.Controladoras
             return resultado;
         }
 
+        /*
+         Método que solicita a la controladora de BD todos los nombres de un módulo, con id del sprint
+         */
         public DataTable getNombresModulo(int idSprint)
         {
             return controladoraBDEstructura.getModulo(idSprint);
--- SAPR/SAPR/App_Code/Entidades/EntidadModulo.cs
+++ SAPR/SAPR/App_Code/Entidades/EntidadModulo.cs
@@ -5,23 +5,34 @@ using System.Web;
 
 namespace SAPR.App_Code.Entidades
 {
+    /*
+     Clase entidad para encapsular los datos de un módulo
+     */
     public class EntidadModulo
     {
         private String nombre;
         private String descripcion;
 
+        /*
+         Constructor que guarda los datos recibidos en el objeto vector
+         */
         public EntidadModulo(Object[] datos)
         {
             this.nombre = datos[0].ToString();
             this.descripcion = datos[1].ToString();
         }
 
+        /*
+         Get y set del nombre del módulo
+         */
         public String Nombre
         {
             get { return nombre; }
             set { nombre = value; }
         }
-
+         /*
+          Get y set de la descripción del módulo
+          */
         public String Descripcion
         {
             get { return descripcion; }
